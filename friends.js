236bce00f2c7ab98f57e8aa635770e7ca2a3171f
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-08 10:12:49 -0600

    Add an array of places

--- friends.js
+++ friends.js
@@ -3,3 +3,8 @@ var arrays = ["Miranda","Maddy","Bre","Greg","Jackson"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["Florida","Colorado","Hawaii","Texas","California"];
+places.unshift("Oregon");
+
+console.log(places);
